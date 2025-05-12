81aebd9856e434ba2f2214538f4619e8458f5982
tqdat410 <tqdat410@gmail.com>
2025-05-12 16:06:00 +0700

    Add database connection timeout & Ping API

---
+++ src/main/java/portfolio/dattq/controller/PingController.java
@@ -0,0 +1,17 @@
+package portfolio.dattq.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ping")
+public class PingController {
+
+    @GetMapping
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("Pong");
+    }
+
+}
